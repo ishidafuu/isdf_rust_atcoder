8b7c02b6267fb01c50ef86bc7c73bb7dbe1f062c
ishidafuu <ishidafuu@gmail.com>
2023-09-08 07:41:47 +0900

    a03,b03

--- tessoku-book/src/bin/a03.rs
+++ tessoku-book/src/bin/a03.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: i32,
+        k: i32,
+        p:[i32;n],
+        q:[i32;n],
+    }
+
+    for ps in &p {
+        for qs in &q {
+            if ps + qs == k {
+                println!("Yes");
+                return;
+            }
+        }
+    }
+
+    println!("No");
 }
--- tessoku-book/src/bin/b03.rs
+++ tessoku-book/src/bin/b03.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: i32,
+        a:[i32;n],
+    }
+
+    for a0 in &a {
+        for a1 in &a {
+            for a2 in &a {
+                if a0 + a1 + a2 == 1000 {
+                    println!("Yes");
+                    return;
+                }
+            }
+        }
+    }
+
+    println!("No");
 }
