6b6b835e6a50656f2dbf78e20cf396e796744a50
ishidafuu <ishidafuu@gmail.com>
2023-09-08 23:22:58 +0900

    b04

--- tessoku-book/src/bin/b04.rs
+++ tessoku-book/src/bin/b04.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: i32,
+    }
+
+    let mut a = n;
+    let mut result = 0;
+
+    for i in 0..8 {
+        if a % 10 == 1 {
+            result += 1 << i;
+        }
+        a /= 10;
+    }
+
+    println!("{}", result);
 }
