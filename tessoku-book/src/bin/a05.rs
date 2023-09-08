4ed0f4970647362be245eb64bdd9827a06c1c0e4
ishidafuu <ishidafuu@gmail.com>
2023-09-08 23:49:17 +0900

    a05

--- tessoku-book/src/bin/a05.rs
+++ tessoku-book/src/bin/a05.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: i32,// 1..3000
+        k: i32,// 3..9000
+    }
+
+    let mut count = 0;
+    for r in 1..=n {
+        if k > r + n + n {
+            continue;
+        }
+        for b in 1..=n {
+            let w = k - r - b;
+            if w >= 1 && w <= n {
+                count += 1;
+            }
+        }
+    }
+
+
+    println!("{}", count);
 }
