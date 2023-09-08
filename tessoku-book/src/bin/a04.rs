18c12db6045d9333ed7a2b154eb0eed2d9113e03
ishidafuu <ishidafuu@gmail.com>
2023-09-08 22:55:10 +0900

    a04

--- tessoku-book/src/bin/a04.rs
+++ tessoku-book/src/bin/a04.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: i32,
+    }
+
+    let mut a = n;
+    let mut result = [0; 10];
+
+    for i in 0..10 {
+        let p = 1 << (10 - 1 - i);
+        if a >= p {
+            a -= p;
+            result[i] = 1;
+        } else {
+            result[i] = 0;
+        }
+    }
+
+    let result_str: Vec<String> = result.iter().map(|&n| n.to_string()).collect();
+    let joined = result_str.join("");
+    println!("{}", joined);
 }
