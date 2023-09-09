bf3dd81b561455819e8f4c58b691ebf5cf8141ae
ishidafuu <ishidafuu@gmail.com>
2023-09-09 10:56:14 +0900

    b06,a07

--- tessoku-book/src/bin/a07.rs
+++ tessoku-book/src/bin/a07.rs
@@ -1,3 +1,36 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        D: usize, // D日間
+        N: usize, // N人
+        LR: [[usize; 2]; N], // L日目からR日目まで出席
+    }
+
+    // 各日の出席者数を出力
+
+    // 各日の人数変化量
+    let mut delta = vec![0; D];
+
+    for i in 0..N
+    {
+        let in_day_index = LR[i][0] - 1;
+        let out_day_index = LR[i][1];
+        // println!("in_day{}", in_day);
+        // println!("out_day{}", out_day);
+        delta[in_day_index] += 1;
+        if out_day_index < D {
+            delta[out_day_index] -= 1;
+        }
+    }
+
+
+    let mut total = 0;
+    for i in 0..D
+    {
+        // println!("delta[i]{}", delta[i]);
+        total += delta[i];
+
+        println!("{}", total);
+    }
 }
--- tessoku-book/src/bin/b06.rs
+++ tessoku-book/src/bin/b06.rs
@@ -1,3 +1,37 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [i32;n],
+        q: usize,
+        lr: [[i32; 2]; q],
+    }
+
+
+    let mut sum = vec![0; n];
+
+    sum[0] = a[0];
+    for i in 1..n {
+        sum[i] = sum[i - 1] + a[i];
+    }
+
+    for i in 0..q {
+        let l = lr[i][0] as usize;
+        let r = lr[i][1] as usize;
+        let total = (r - l + 1) as i32;
+        let hit = if l == 1 { sum[r - 1] } else { sum[r - 1] - sum[l - 2] };
+        let miss = (total - hit) as i32;
+        // println!("total{}", total);
+        // println!("sum[r - 1]{}", sum[r - 1]);
+        // if l != 1 { println!("sum[l - 2]{}", sum[l - 2]); }
+        // println!("hit{} miss{}", hit, miss);
+        if hit > miss {
+            println!("win");
+        } else if hit < miss {
+            println!("lose");
+        } else {
+            println!("draw");
+        }
+    }
 }
